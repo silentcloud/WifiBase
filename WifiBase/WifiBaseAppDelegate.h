b591b3bf1229ccbc9c5d9360cdfe1feebbb916a4
qiang.mou <qiang.mou@alipay.com>
2013-11-24 17:36:53 +0800

    Initial Commit

---
+++ WifiBase/WifiBaseAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WifiBaseAppDelegate.h
+//  WifiBase
+//
+//  Created by silentcloud on 11/24/13.
+//  Copyright (c) 2013 silentcloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WifiBaseAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WifiBase/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WifiBase
+//
+//  Created by silentcloud on 11/24/13.
+//  Copyright (c) 2013 silentcloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WifiBaseAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WifiBaseAppDelegate class]));
+    }
+}
